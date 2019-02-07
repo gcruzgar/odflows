1ab18a1be08c352d1f25db96a4ed9d5e2cf82e97
gcruzgar <g.cruz@leeds.ac.uk>
2019-02-07 14:35:25 +0000

    creating script to read Zoopla data

---
+++ scripts/zoopla_data.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+
+rentals = pd.read_csv("data/ZooplaRentals_Aggregate_NikLomax.txt", sep='\t')
+sales = pd.read_csv("data/ZooplaSales_Aggregate_NikLomax.txt", sep='\t')    
+    
