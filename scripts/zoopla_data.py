dfd54df918f07764b264a729763ab7c6b99475fb
gcruzgar <g.cruz@leeds.ac.uk>
2019-02-08 11:40:34 +0000

    changing into a function and working on plots

--- scripts/zoopla_data.py
+++ scripts/zoopla_data.py
@@ -1,31 +1,53 @@
 #!/usr/bin/env python3
-import pandas as pd 
-import matplotlib.pyplot as plt 
+import argparse
 import numpy as np 
+import pandas as pd 
 import geopandas as gpd 
 from utils import uk_plot
+import matplotlib.pyplot as plt 
 
-rentals = pd.read_csv("data/ZooplaRentals_Aggregate_NikLomax.txt", sep='\t')
-sales = pd.read_csv("data/ZooplaSales_Aggregate_NikLomax.txt", sep='\t')    
+def main():
 
-#rentals.iloc[:,4:].sum()
-#sales.iloc[:,4:].sum()  
+    var_name = args.var_name
+    
+    #rentals = pd.read_csv("data/ZooplaRentals_Aggregate_NikLomax.txt", sep='\t')
+    sales = pd.read_csv("data/ZooplaSales_Aggregate_NikLomax.txt", sep='\t')    
 
-sales_types = ['NumberOfMoves', 'MovesUnder250k', 'MovesOver250k', 
-    'Terraced', 'Flat', 'SemiDetached', 'Detached',
-    'Beds1to3', 'Beds4Plus']
-rental_types = ['NumberOfRentals', 'RentUnder250', 'RentOver250',
-    'Terraced', 'Flat', 'SemiDetached', 'Detached', 'Bungalow', 'PropertyTypeUnknown', 
-    'Beds1to3', 'Beds4Plus']
+    #rentals.iloc[:,4:].sum()
+    #sales.iloc[:,4:].sum()  
 
-sales_origin = pd.pivot_table(sales, values = sales_types, index = 'OriginWardName', aggfunc=np.sum) 
-sales_destination = pd.pivot_table(sales, values = sales_types, index = 'DestinationWardName', aggfunc=np.sum)
+    sales_types = ['NumberOfMoves', 'MovesUnder250k', 'MovesOver250k', 
+        'Terraced', 'Flat', 'SemiDetached', 'Detached',
+        'Beds1to3', 'Beds4Plus']
+    #rental_types = ['NumberOfRentals', 'RentUnder250', 'RentOver250',
+    #    'Terraced', 'Flat', 'SemiDetached', 'Detached', 'Bungalow', 'PropertyTypeUnknown', 
+    #    'Beds1to3', 'Beds4Plus']
 
-rentals_origin = pd.pivot_table(rentals, values = rental_types, index = 'OriginWardName', aggfunc=np.sum) 
-rentals_destination = pd.pivot_table(rentals, values = rental_types, index = 'DestinationWardName', aggfunc=np.sum)   
+    # Plots
+    #if
+    df_dict = {'sales_origin': pd.pivot_table(sales, values = sales_types, index = 'OriginWardName', aggfunc=np.sum), 
+        'sales_destination': pd.pivot_table(sales, values = sales_types, index = 'DestinationWardName', aggfunc=np.sum),
+    }
+    #df_dict['sales_net'] =  df_dict['sales_destination'] - df_dict['sales_origin'] 
+    # df_dict = {
+    #     'rentals_origin': pd.pivot_table(rentals, values = rental_types, index = 'OriginWardName', aggfunc=np.sum), 
+    #     'rentals_destination': pd.pivot_table(rentals, values = rental_types, index = 'DestinationWardName', aggfunc=np.sum)
+    # }
 
-shp_path = "data/shapefiles/UK_Wards_2016.shp"
-df = sales_origin
-var_name = "NumberOfMoves"
-title = "Number of moves originating from each ward"
-uk_plot(shp_path, df, var_name, title)
+    shp_path = "data/shapefiles/UK_Wards_2016.shp"
+
+    for key in df_dict:
+        
+        df = df_dict[key]
+        title = key+' - '+var_name
+        uk_plot(shp_path, df, var_name, title)
+    plt.show()
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("var_name", type=str, nargs='?', default="NumberOfMoves",
+        help="Variable to plot, e.g. 'NumberOfMoves' or 'Beds1to3'.")
+    args = parser.parse_args()
+    
+    main()
