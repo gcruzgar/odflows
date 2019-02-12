5d04be43cf9583ba18e3afdfef6284dd7a2b6a9b
gcruzgar <g.cruz@leeds.ac.uk>
2019-02-12 14:35:15 +0000

    changing script to function and adding arguments

--- scripts/od_pairs.py
+++ scripts/od_pairs.py
@@ -6,18 +6,31 @@ import geopandas as gpd
 from utils import uk_plot, top_10
 import matplotlib.pyplot as plt 
 
-sales = pd.read_csv("data/ZooplaSales_Aggregate_NikLomax.txt", sep='\t').dropna(axis=0, how='any')
+def main():
 
-sales_types = ['NumberOfMoves', 'MovesUnder250k', 'MovesOver250k', 
-    'Terraced', 'Flat', 'SemiDetached', 'Detached',
-    'Beds1to3', 'Beds4Plus']
+    var_name = args.var_name
 
-sales_origin = pd.pivot_table(sales, values = sales_types, index = 'OriginWardName', aggfunc=np.sum)
-sales_destination = pd.pivot_table(sales, values = sales_types, index = 'DestinationWardName', aggfunc=np.sum)
-sales_net = sales_destination - sales_origin
+    sales = pd.read_csv("data/ZooplaSales_Aggregate_NikLomax.txt", sep='\t').dropna(axis=0, how='any')
 
-shp_path = "data/shapefiles/UK_Wards_2016.shp"
-var_name = 'NumberOfMoves'
-uk_plot(shp_path, sales_net, var_name, 'sales - '+var_name)
-top_10(sales_net,var_name, var_name+' (net) - Top 10')
-plt.show()
+    sales_types = ['NumberOfMoves', 'MovesUnder250k', 'MovesOver250k', 
+        'Terraced', 'Flat', 'SemiDetached', 'Detached',
+        'Beds1to3', 'Beds4Plus']
+
+    sales_origin = pd.pivot_table(sales, values = sales_types, index = 'OriginWardName', aggfunc=np.sum)
+    sales_destination = pd.pivot_table(sales, values = sales_types, index = 'DestinationWardName', aggfunc=np.sum)
+    sales_net = sales_destination - sales_origin
+
+    shp_path = "data/shapefiles/UK_Wards_2016.shp"
+
+    uk_plot(shp_path, sales_net, var_name, 'sales - '+var_name)
+    top_10(sales_net,var_name, var_name+' (net) - Top 10')
+    plt.show()
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("var_name", type=str, nargs='?', default="NumberOfMoves",
+        help="Variable to plot, e.g. 'NumberOfMoves' or 'Beds1to3'.")
+    args = parser.parse_args()
+
+    main()
