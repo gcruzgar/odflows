38e7a55ccdb92e1366a20901d8a52c88563bfeaa
gcruzgar <g.cruz@leeds.ac.uk>
2019-02-15 14:08:36 +0000

    looping over all values + using uk shp

--- scripts/distance.py
+++ scripts/distance.py
@@ -2,20 +2,25 @@
 import pandas as pd 
 import geopandas as gpd 
 
-sales = pd.read_csv("data/ZooplaSales_Aggregate_NikLomax.txt", sep='\t')            # data
-map_df = gpd.read_file("data/shapefiles/GB_Wards_2015.shp")                         # shapefile
+sales = pd.read_csv("data/ZooplaSales_Aggregate_NikLomax.txt", sep='\t').dropna()            # data
+map_df = gpd.read_file("data/shapefiles/UK_Wards_2016.shp")                         # shapefile
 
 #merged = map_df.set_index("wd15nm").join(sales.set_index('OriginWardName')).dropna(subset=['NumberOfMoves'])  # data + shp
 
-a = 'E05004153'
-ai = map_df.loc[map_df['wd15cd']==a].index   # find index of first ward
-ma = map_df['geometry'][ai.item()]           # find geometry of first ward
-mac = ma.centroid                            # centre point of first ward
+dist_list = []
+for i in sales.index:
 
-b = 'E05009501'
-bi = map_df.loc[map_df['wd15cd']==b].index   # find index of second ward
-mb = map_df['geometry'][bi.item()]           # find geometry of second ward
-mbc = mb.centroid                            # centre point of second ward
+    a = sales['OriginWardCode'][i]               # ward code
+    ai = map_df.loc[map_df['wd16cd']==a].index   # find index of first ward
+    ma = map_df['geometry'][ai.item()]           # find geometry of first ward
+    mac = ma.centroid                            # centre point of first ward
 
-ab_dist = ma.distance(mb)                    # distance between wards 
-print(ab_dist)
+    b = sales['DestinationWardCode'][i]          # ward code
+    bi = map_df.loc[map_df['wd16cd']==b].index   # find index of second ward
+    mb = map_df['geometry'][bi.item()]           # find geometry of second ward       
+    mbc = mb.centroid                            # centre point of second ward
+
+    ab = mac.distance(mbc)                       # distance between centroids
+    dist_list.append(ab)             
+
+sales['distance'] = dist_list
