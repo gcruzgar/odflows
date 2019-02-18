0ecf2778198ba1ddd9a9ec2b1411a264bb707930
gcruzgar <g.cruz@leeds.ac.uk>
2019-02-18 14:18:30 +0000

    now works with rentals (-r)

--- scripts/zoopla_data.py
+++ scripts/zoopla_data.py
@@ -10,24 +10,27 @@ def main():
 
     var_name = args.var_name
 
-    sales = pd.read_csv("data/ZooplaSales_Aggregate_NikLomax.txt", sep='\t')    
+    if args.r:
+        df = pd.read_csv("data/ZooplaRentals_Aggregate_NikLomax.txt", sep='\t')
+        df.rename(index=str, columns={'NumberOfRentals': 'Total'}, inplace=True)
+        df_types = ['Total', 'RentUnder250', 'RentOver250',
+            'Terraced', 'Flat', 'SemiDetached', 'Detached', 'Bungalow', 'PropertyTypeUnknown', 
+            'Beds1to3', 'Beds4Plus']
+    else:
+        df = pd.read_csv("data/ZooplaSales_Aggregate_NikLomax.txt", sep='\t')
+        df.rename(index=str, columns={'NumberOfMoves': 'Total'}, inplace=True)   
+        df_types = ['Total', 'MovesUnder250k', 'MovesOver250k', 
+            'Terraced', 'Flat', 'SemiDetached', 'Detached',
+            'Beds1to3', 'Beds4Plus']
 
-    print("Total frequency: \n{}".format(sales.iloc[:,4:].sum())) 
+    print("Total frequency: \n{}".format(df.iloc[:,4:].sum())) 
 
-    sales_types = ['NumberOfMoves', 'MovesUnder250k', 'MovesOver250k', 
-        'Terraced', 'Flat', 'SemiDetached', 'Detached',
-        'Beds1to3', 'Beds4Plus']
-
-    df_dict = {'sales_origin': pd.pivot_table(sales, values = sales_types, index = 'OriginWardName', aggfunc=np.sum), 
-        'sales_destination': pd.pivot_table(sales, values = sales_types, index = 'DestinationWardName', aggfunc=np.sum),
+    df_dict = {'df_origin': pd.pivot_table(df, values = df_types, index = 'OriginWardName', aggfunc=np.sum), 
+        'df_destination': pd.pivot_table(df, values = df_types, index = 'DestinationWardName', aggfunc=np.sum),
     }
-    #df_dict['sales_net'] =  df_dict['sales_destination'] - df_dict['sales_origin'] 
 
-    print("\nMoves from origin: \n{}".format(df_dict['sales_origin'].sum()))
-    print("\nMoves to destination: \n{}".format(df_dict['sales_destination'].sum()))
-
-    #a = sales.loc[sales['OriginWardName'] == "Epping Hemnall"]
-    #b = sales.loc[sales['DestinationWardName'] == "Epping Hemnall"]
+    print("\nMoves from origin: \n{}".format(df_dict['df_origin'].sum()))
+    print("\nMoves to destination: \n{}".format(df_dict['df_destination'].sum()))
 
     # Plots
     shp_path = "data/shapefiles/GB_Wards_2015.shp"
@@ -39,37 +42,23 @@ def main():
         uk_plot(shp_path, df, var_name, title)
         #top_10(df,var_name, title)
 
-    if var_name != 'NumberOfMoves':
-        pc_sales_origin =  df_dict['sales_origin'].drop(columns=['NumberOfMoves']).div(df_dict['sales_origin']['NumberOfMoves'], axis=0)
-        pc_sales_destination = df_dict['sales_destination'].drop(columns=['NumberOfMoves']).div(df_dict['sales_destination']['NumberOfMoves'], axis=0)
+    if var_name != 'Total':
+        pc_df_origin =  df_dict['df_origin'].drop(columns=['Total']).div(df_dict['df_origin']['Total'], axis=0)
+        pc_df_destination = df_dict['df_destination'].drop(columns=['Total']).div(df_dict['df_destination']['Total'], axis=0)
 
-        uk_plot(shp_path, pc_sales_origin, var_name, 'sales_origin - '+var_name+' - net normalised')
-        uk_plot(shp_path, pc_sales_destination, var_name, 'sales_destination - '+var_name+' - net normalised')
-        #top_10(pc_sales_net,var_name, var_name+' (net normalised) - Top 10')
+        uk_plot(shp_path, pc_df_origin, var_name, 'df_origin - '+var_name+' - net normalised')
+        uk_plot(shp_path, pc_df_destination, var_name, 'df_destination - '+var_name+' - net normalised')
+        #top_10(pc_df_net,var_name, var_name+' (net normalised) - Top 10')
 
     plt.show()
-    
-    ##rentals
-
-    #rentals = pd.read_csv("data/ZooplaRentals_Aggregate_NikLomax.txt", sep='\t')
-
-    #print(rentals.iloc[:,4:].sum())
-
-    #rental_types = ['NumberOfRentals', 'RentUnder250', 'RentOver250',
-    #    'Terraced', 'Flat', 'SemiDetached', 'Detached', 'Bungalow', 'PropertyTypeUnknown', 
-    #    'Beds1to3', 'Beds4Plus']
-
-    # df_dict = {
-    #     'rentals_origin': pd.pivot_table(rentals, values = rental_types, index = 'OriginWardName', aggfunc=np.sum), 
-    #     'rentals_destination': pd.pivot_table(rentals, values = rental_types, index = 'DestinationWardName', aggfunc=np.sum)
-    # }
-
 
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("var_name", type=str, nargs='?', default="NumberOfMoves",
-        help="Variable to plot, e.g. 'NumberOfMoves' or 'Beds1to3'.")
+    parser.add_argument("var_name", type=str, nargs='?', default="Total",
+        help="Variable to plot, e.g. 'Total' or 'Beds1to3'.")
+    parser.add_argument("-r", action='store_true',
+        help="use rental data.")
     args = parser.parse_args()
     
     main()
