24e73f73885109df7eb774c1607a8bd7df73eeeb
gcruzgar <g.cruz@leeds.ac.uk>
2019-02-19 15:41:20 +0000

    rewriting script as function

--- scripts/mobility.py
+++ scripts/mobility.py
@@ -1,46 +1,58 @@
 #!/usr/bin/env python3
+import argparse
 import pandas as pd 
 import geopandas as gpd 
 import matplotlib.pyplot as plt 
 from utils import uk_plot, categ_plot
 
-rentals = pd.read_csv("data/rentals_and_distance.csv", index_col=0)
-sales = pd.read_csv("data/sales_and_distance.csv", index_col=0)
+def main():
 
-sales_types = {
-    'beds': ['Beds1to3', 'Beds4Plus'], 
-    'dwelling': ['Terraced', 'Flat', 'SemiDetached', 'Detached'], 
-    'price': ['MovesUnder250k', 'MovesOver250k']
-}
+    if args.r:
+        df = pd.read_csv("data/rentals_and_distance.csv", index_col=0)
+        df_types = {
+            'beds': ['Beds1to3', 'Beds4Plus'], 
+            'dwelling': ['Terraced', 'Flat', 'SemiDetached', 'Detached', 'Bungalow'], 
+            'price': ['RentUnder250', 'RentOver250']
+        }
+    else:
+        df = pd.read_csv("data/sales_and_distance.csv", index_col=0)
+        df_types = {
+            'beds': ['Beds1to3', 'Beds4Plus'], 
+            'dwelling': ['Terraced', 'Flat', 'SemiDetached', 'Detached'], 
+            'price': ['MovesUnder250k', 'MovesOver250k']
+        }
 
-rentals_types = {
-    'beds': ['Beds1to3', 'Beds4Plus'], 
-    'dwelling': ['Terraced', 'Flat', 'SemiDetached', 'Detached', 'Bungalow'], 
-    'price': ['RentUnder250', 'RentOver250']
-}
+    # most common type in each ward
+    df['dwelling'] = df[df_types['dwelling']].idxmax(axis=1)
 
-# most common type in each ward
-sales['dwelling'] = sales[sales_types['dwelling']].idxmax(axis=1)
+    # filter by category
+    d = {}
+    for cat in df_types['dwelling']:
+        d[cat] = df.loc[df['dwelling'] == cat, 'distance'].mean()
+    distance_df = pd.Series(d)
+    print("Average distance (m): \n{}".format(distance_df.round(2)))
 
-# filter by category
-distance_list = []
-for cat in sales_types['dwelling']:
-    distance_list.append((cat, [sales.loc[sales['dwelling'] == cat, 'distance'].mean()]))
-distance_list = pd.DataFrame.from_items(distance_list)
-print("Average distance (m): \n{}".format(distance_list.round(2)))
+    #rural/urban classification
+    rural_urban = pd.read_csv("data/Rural_Urban_Classification_2011_of_Wards_in_England_and_Wales.csv")
+    #print(rural_urban['RUC11'].value_counts())
 
-#rural/urban classification
-rural_urban = pd.read_csv("data/Rural_Urban_Classification_2011_of_Wards_in_England_and_Wales.csv")
-#print(rural_urban['RUC11'].value_counts())
+    ru_map = {
+        'Rural village and dispersed': 'Rural', 'Rural village and dispersed in a sparse setting': 'Rural', 
+        'Rural town and fringe': 'Rural', 'Rural town and fringe in a sparse setting': 'Rural',
+        'Urban major conurbation':'Urban', 'Urban minor conurbation':'Urban',
+        'Urban city and town':'Urban', 'Urban city and town in a sparse setting': 'Urban'
+    }
+    rural_urban['RUC11'].replace(ru_map, inplace=True)
 
-ru_map = {
-    'Rural village and dispersed': 'Rural', 'Rural village and dispersed in a sparse setting': 'Rural', 
-    'Rural town and fringe': 'Rural', 'Rural town and fringe in a sparse setting': 'Rural',
-    'Urban major conurbation':'Urban', 'Urban minor conurbation':'Urban',
-    'Urban city and town':'Urban', 'Urban city and town in a sparse setting': 'Urban'
-}
-rural_urban['RUC11'].replace(ru_map, inplace=True)
+    shp_path = "data/shapefiles/GB_Wards_2015.shp"
+    categ_plot(shp_path, rural_urban.set_index('WD11NM'), 'RUC11', 'Rural/Urban split - 2011')
+    plt.show()
 
-shp_path = "data/shapefiles/GB_Wards_2015.shp"
-categ_plot(shp_path, rural_urban.set_index('WD11NM'), 'RUC11', 'Rural/Urban split - 2011')
-plt.show()
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", action='store_true',
+        help="use rental data.")
+    args = parser.parse_args()
+
+    main()
