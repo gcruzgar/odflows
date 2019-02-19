5375cacd0c82f8eff5c1d9f71021c33ba2306629
gcruzgar <g.cruz@leeds.ac.uk>
2019-02-19 14:36:58 +0000

    rural-urban classification

--- scripts/mobility.py
+++ scripts/mobility.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import pandas as pd 
 import geopandas as gpd 
 import matplotlib.pyplot as plt 
@@ -24,5 +25,22 @@ sales['dwelling'] = sales[sales_types['dwelling']].idxmax(axis=1)
 # filter by category
 distance_list = []
 for cat in sales_types['dwelling']:
-    distance_list.append(sales.loc[sales['dwelling'] == cat, 'distance'].mean())
-print(list(zip(sales_types['dwelling'], distance_list)))
+    distance_list.append((cat, [sales.loc[sales['dwelling'] == cat, 'distance'].mean()]))
+distance_list = pd.DataFrame.from_items(distance_list)
+print("Average distance (m): \n{}".format(distance_list.round(2)))
+
+#rural/urban classification
+rural_urban = pd.read_csv("data/Rural_Urban_Classification_2011_of_Wards_in_England_and_Wales.csv")
+#print(rural_urban['RUC11'].value_counts())
+
+ru_map = {
+    'Rural village and dispersed': 'Rural', 'Rural village and dispersed in a sparse setting': 'Rural', 
+    'Rural town and fringe': 'Rural', 'Rural town and fringe in a sparse setting': 'Rural',
+    'Urban major conurbation':'Urban', 'Urban minor conurbation':'Urban',
+    'Urban city and town':'Urban', 'Urban city and town in a sparse setting': 'Urban'
+}
+rural_urban['RUC11'].replace(ru_map, inplace=True)
+
+shp_path = "data/shapefiles/GB_Wards_2015.shp"
+categ_plot(shp_path, rural_urban.set_index('WD11NM'), 'RUC11', 'Rural/Urban split - 2011')
+plt.show()
