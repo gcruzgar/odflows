01c3d516c42f0ff2bd4c4d6b7269db1c67d85cfd
gcruzgar <g.cruz@leeds.ac.uk>
2019-02-20 14:08:36 +0000

    correlation of each category with distance

--- scripts/correlations.py
+++ scripts/correlations.py
@@ -1,43 +1,36 @@
 #!/usr/bin/env python3
 import pandas as pd 
-import numpy as np 
+import argparse
 
-df = pd.read_csv("data/UKvarR.csv")
-#df.head()
-#df.columns
-
-odm = pd.pivot_table(df, values = 'Flow', index = 'Dest', columns = ['Ori'], fill_value=0)
-total_out = odm.sum(axis=0).rename('total_out') 
-total_in = odm.sum(axis=1).rename('total_in') 
-
-var_list = ['Ori', 'Dest', 'Flow', 'ContiguityDum', 'Offset', 'Distance', 'OriPop',
-       'DestPop', 'OriDens', 'DestDens', 'PcUnempOri', 'PcUnempDest',
-       'PcRentOri', 'PcRentDest', 'PcNonNatOri', 'PcNonNatDest', 'PcDegreeOri',
-       'PcDegreeDest', 'PcIllOri', 'PcIllDest', 'DepRatioOri', 'DepRatioDest',
-       'PcMarriedOri', 'PcMarriedDest']
-
-attraction_vars = ['DestPop', 'DestDens', 'PcUnempDest', 'PcRentDest', 'PcNonNatDest',
-       'PcDegreeDest', 'PcIllDest', 'DepRatioDest', 'PcMarriedDest']
-
-emission_vars = ['OriPop', 'OriDens', 'PcUnempOri', 'PcRentOri', 'PcNonNatOri',
-       'PcDegreeOri', 'PcIllOri', 'DepRatioOri', 'PcMarriedOri']
-
-print("\ncorrelation of destination factors with migration flow to destination:")
-for att in attraction_vars:
+def main():
     
-    att_df = pd.pivot_table(df, values = att, index = 'Dest')
-    f_df = pd.concat([total_in, att_df], axis=1, join='inner')
-    fc = f_df.corr()
-    print("%s: %0.3f" % (att, fc.loc[att][0]))
+    if args.r:
+        r = 'Rentals'
+        df = pd.read_csv("data/rentals_and_distance.csv", index_col=0)
 
-print("\ncorrelation of origin factors with flow from origin:")
-for em in emission_vars:
+        var_list=['NumberOfRentals', 'RentUnder250', 'RentOver250',
+            'Terraced', 'Flat', 'SemiDetached', 'Detached', 'Bungalow',
+            'PropertyTypeUnknown', 'Beds1to3', 'Beds4Plus', 'distance']
+    else:
+        r = 'Sales'    
+        df = pd.read_csv("data/sales_and_distance.csv", index_col=0)
+
+        var_list = ['NumberOfMoves', 'MovesUnder250k', 'MovesOver250k',
+            'Terraced', 'Flat', 'SemiDetached', 'Detached',
+            'Beds1to3', 'Beds4Plus', 'distance']
+
+    print("\nCorrelation of %s with distance: " % r)
+    df_cor = df[var_list].corr()
+
+    print(df_cor['distance'].round(3)[:-1])
+
+    return df_cor
+
+if __name__ == "__main__":
     
-    em_df = pd.pivot_table(df, values = em, index = 'Ori')
-    f_df = pd.concat([total_out, em_df], axis=1, join='inner')
-    fc = f_df.corr()
-    print("%s: %0.3f" % (em, fc.loc[em][0]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", action='store_true',
+        help="use rental data.")    
+    args = parser.parse_args()
 
-a = df[['Distance', 'Flow']]
-a = a.loc[a['Flow'] != 0]
-print("\nDistance: %0.3f" % a.corr().iloc[0,1])
+    main()
