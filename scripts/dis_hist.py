a2e74b04d7de3d4eb986b27da72f505cfa9e17d2
gcruzgar <g.cruz@leeds.ac.uk>
2019-02-28 15:02:08 +0000

    option for rentals and for rural_urban

--- scripts/dis_hist.py
+++ scripts/dis_hist.py
@@ -6,7 +6,6 @@ df_types = {
     'beds': ['Beds1to3', 'Beds4Plus'], 
     'dwelling': ['Terraced', 'Flat', 'SemiDetached', 'Detached', 'Bungalow'], 
     'price': ['RentUnder250', 'RentOver250'],
-    'RUC11': ['Rural', 'Urban']
 }
 
 # df = pd.read_csv("data/sales_and_distance.csv", index_col=0)
@@ -16,18 +15,24 @@ df_types = {
 #     'price': ['MovesUnder250k', 'MovesOver250k'],
 # }
 
+# df = pd.read_csv("data/sales_distance_ru.csv")
+# df_types= {
+#     'RUC11': ['Rural', 'Urban']
+# }
+
 d={}
 i=1
 for cat in df_types['dwelling']:
     
-    plt.subplot(3,2,i)
-    _=plt.hist(df['distance']/(1000), bins=300 ,weights=df[cat], cumulative=False, normed=True)
-    plt.xlim([0, 80])
-    plt.xlabel("Distance (km)")
-    plt.ylabel("Frequency (%)")
-    plt.title(cat)
+    #df1 = df.loc[df['RUC11']==cat]
+    _=plt.subplot(3,2,i)
+    _=plt.hist(df['distance']/(1000), bins=500 ,weights=df[cat], cumulative=False, normed=True) #weights=df['NumberOfMoves'] if RUC11
+    _=plt.xlim([0, 80])
+    _=plt.xlabel("Distance (km)")
+    _=plt.ylabel("Frequency (%)")
+    _=plt.title(cat)
 
-    d[cat] = (df['distance']*df[cat]).mean()
+    d[cat] = (df['distance']*df[cat]).mean() #'NumberOfMoves'
 
     i+=1
 
