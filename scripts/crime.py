2c5de1ee1bfbf61fbd619ae89197d146ac05a230
gcruzgar <g.cruz@leeds.ac.uk>
2019-03-22 13:28:14 +0000

    reformatting as function

--- scripts/crime.py
+++ scripts/crime.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import numpy as np 
 import pandas as pd 
 from os import listdir
@@ -6,33 +7,49 @@ import geopandas as gpd
 import matplotlib.pyplot as plt 
 from utils import uk_plot
 
-LSOA_Ward = pd.read_csv("data/EW_LSOA_Ward_Lookup_2016.csv") # LSOA to Ward lookup
+def main():
 
-data_path = "data/crime/"
-folder_list = listdir(data_path)
+    crime_type = args.c[0] 
 
-crime = pd.DataFrame()
-for folder_name in folder_list:
+    LSOA_Ward = pd.read_csv("data/EW_LSOA_Ward_Lookup_2016.csv") # LSOA to Ward lookup
 
-    file_list = listdir(data_path+folder_name)
+    data_path = "data/crime/"
+    folder_list = listdir(data_path)
 
-    print("Loading %s data..." % folder_name)
-    for file_name in file_list:
+    crime = pd.DataFrame()
+    for folder_name in sorted(folder_list):
 
-        f_crime = pd.read_csv(data_path+folder_name+r'/'+file_name) #load crime data for sepcific file 
+        file_list = listdir(data_path+folder_name)
 
-        f_crime = f_crime[['LSOA code', 'Crime type']] # only interested in crime type and location
-        f_crime = f_crime.loc[f_crime['Crime type'] == 'Burglary'] # filter by crime type
+        print("Loading %s data..." % folder_name)
+        for file_name in file_list:
 
-        crime = pd.concat([crime, f_crime], axis=0) # join data from all files
+            f_crime = pd.read_csv(data_path+folder_name+r'/'+file_name) #load crime data for sepcific file 
 
-crime_rate = pd.DataFrame(crime['LSOA code'].value_counts()).rename(index=str, columns={'LSOA code': 'Crime rate'}) # number of crimes per LSOA
-crime_conv = crime_rate.merge(LSOA_Ward, left_on=crime_rate.index, right_on='LSOA11CD', how='right').fillna(value=0)
+            f_crime = f_crime[['LSOA code', 'Crime type']] # only interested in crime type and location
+            f_crime = f_crime.loc[f_crime['Crime type'] == crime_type ] # filter by crime type
 
-crime_ward = pd.pivot_table(crime_conv, values=['Crime rate'], index='WD16CD', aggfunc=np.sum)
+            crime = pd.concat([crime, f_crime], axis=0) # join data from all files
 
-shp_path = "data/shapefiles/GB_Wards_2016.shp"
-geo_code = 'wd16cd'
+    crime_rate = pd.DataFrame(crime['LSOA code'].value_counts()).rename(index=str, columns={'LSOA code': 'Crime rate'}) # number of crimes per LSOA
+    crime_conv = crime_rate.merge(LSOA_Ward, left_on=crime_rate.index, right_on='LSOA11CD', how='right').fillna(value=0)
 
-uk_plot(shp_path, geo_code, crime_ward,'Crime rate', 'average crime rate - burglary', cmap='OrRd')
-plt.show()
+    crime_ward = pd.pivot_table(crime_conv, values=['Crime rate'], index='WD16CD', aggfunc=np.sum)
+
+    shp_path = "data/shapefiles/GB_Wards_2016.shp"
+    geo_code = 'wd16cd'
+
+    uk_plot(shp_path, geo_code, crime_ward,'Crime rate', 'average crime rate - %s' % crime_type, cmap='OrRd') # plot crime data
+    plt.show()
+
+if __name__ == "__main__":
+        
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-r", action='store_true',
+        help="use rental data.")
+    parser.add_argument("-c", type=str, nargs=1, default=["Burglary"], 
+        help="Category to plot: 'beds', 'dwelling' or 'price'.")
+    args = parser.parse_args()
+
+    main()
