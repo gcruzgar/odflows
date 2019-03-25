a68c83ff4007e6ff218fecc5f6648cb8d02a5012
gcruzgar <g.cruz@leeds.ac.uk>
2019-03-25 11:41:27 +0000

    working on correlations with crime

--- scripts/correlations.py
+++ scripts/correlations.py
@@ -26,6 +26,19 @@ def main():
 
     return df_cor
 
+## crime data 
+# crime = pd.read_csv("data/Burglary_LAD_2016.csv")
+# migration = pd.read_csv("data/sales_LAD_2016.csv").drop(columns=['dwelling']).set_index('lad16cd')
+
+# migration_perc = migration.drop(columns=['Total']).div(migration['Total'], axis=0) * 100
+
+# merged = crime.merge(migration, left_on='lad16cd', right_on=migration.index).set_index('lad16cd')
+# merged_perc = crime.merge(migration_perc, left_on='lad16cd', right_on=migration_perc.index).set_index('lad16cd')
+
+# corr_df = merged_perc.corr()
+# print(corr_df.iloc[0].round(3))
+##
+
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
